f2e5069bc201034e55be3af79c61e7842e9c2b4e
kameamea133 <monnier1977@gmail.com>
2024-07-03 12:08:01 +0200

    Create form for adding a new recipe

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
+
 
 const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try{
+        const response = await axios.get('http://localhost:3000/recipes');
+        setRecipes(response.data);
+        console.log(response.data)        
+      }catch(err) {
+        console.log(err)
+      }
+    }
+    fetchRecipe()
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Recipes</h1>
+    </div>
   )
 }
 
