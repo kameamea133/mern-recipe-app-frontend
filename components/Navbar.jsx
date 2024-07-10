9572cd5b5736e9b9d24c3e5d43a3d3514b8c5ce7
kameamea133 <monnier1977@gmail.com>
2024-07-10 14:12:26 +0200

    navbar responsive style added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,15 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import {useCookies} from "react-cookie"
 import { useDispatch } from 'react-redux'
 import { logoutUser } from '../reducers/users'
 import { IoMdLogOut } from "react-icons/io";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 
 const Navbar = () => {
   const [cookies, setCookies] = useCookies(["access_token"]);
-  
+  const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch()
 
 
@@ -18,19 +20,29 @@ const Navbar = () => {
     dispatch(logoutUser());
   }
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className='navbar'>
-        <Link to="/">
-          <img src="/logoRecipe.png" alt="logo" className='logo'/>
-        </Link>
-        
-        <Link to="/create-recipe">Create Recipe</Link>
-        {!cookies.access_token ? ( <Link to="/auth">Login/Register</Link>) : <><Link to="/saved-recipes">My Saved Recipes</Link>
+    <Link to="/">
+      <img src="/logoRecipe.png" alt="logo" className='logo'/>
+    </Link>
+    <div className={`nav-links ${isOpen ? 'open' : ''}`}> {/* Ajout de la classe open */}
+      <Link to="/create-recipe">Create Recipe</Link>
+      {!cookies.access_token ? ( <Link to="/auth">Login/Register</Link>) : (
+        <>
+          <Link to="/saved-recipes">My Saved Recipes</Link>
           <Link to="/my-recipes">My Recipes</Link>
-        <button onClick={logout} className='logout-icon'><IoMdLogOut size={20} />
-</button></>}
-       
+          <button onClick={logout} className='logout-icon'><IoMdLogOut size={20} /></button>
+        </>
+      )}
     </div>
+    <div className='menu-icon' onClick={toggleMenu}>
+      {isOpen ? <FaTimes size={30} /> : <FaBars size={30} />} {/* Affichage des icônes */}
+    </div>
+  </div>
   )
 }
 
